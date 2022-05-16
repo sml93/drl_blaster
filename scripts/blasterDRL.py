7cbd3dec81fd01b718ff8f4bcd3046c7d90207f5
sml93 <shawnmlee93@gmail.com>
2022-05-16 22:55:31 +0800

    First Commit

--- scripts/blasterDRL.py
+++ scripts/blasterDRL.py
@@ -21,3 +21,28 @@ from keras.layers import *
 from keras.optimizers import *
 from keras.models import Sequential
 
+class Intel:
+  def __init__(self, num_state, num_action, RL_GAMMA = 0.99):
+    self.num_state = num_state
+    self.num_action = num_action
+    self.model = self._createModel()
+    self.GAMMA = RL_GAMMA
+
+  def _createModel(self):
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_dim=self.num_state))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(self.num_action,activation='linear'))
+    opt = RMSprop(lr=0.00025)
+    model.compile(loss='mse', optimizer=opt)
+    return model
+
+  def train(self, x, y, batch_size=64, epochs=1, verbose=0):
+    self.model.fit(x, y, batch_size=batch_size, epochs=epochs, verbose=verbose)
+
+  def predict(self, batch_states, verbose=0):
+    return self.model.predict(batch_states, verbose=verbose)
+
+  def predictOne(self, state_test):
+    return self.predict(state_test.reshape(1, self.num_state)).flatten()
+
